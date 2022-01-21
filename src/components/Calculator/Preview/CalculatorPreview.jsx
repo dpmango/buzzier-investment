9b802f0410fd31fe07fe061f4017e61fe58431f6
Ron Dominator <andrei@buzz.fit>
2022-01-21 18:29:07 +0300

    Updating ROI calculations

--- src/components/Calculator/Preview/CalculatorPreview.jsx
+++ src/components/Calculator/Preview/CalculatorPreview.jsx
@@ -10,8 +10,9 @@ import styles from './CalculatorPreview.module.scss';
 import HeadDecorArrowSvg from './assets/decor-arrow.svg';
 import { HomeFact } from 'components/Home';
 
-const InvestmentStep = 5000;
-const maxInvestmentValue = 100 * 1000;
+const InvestmentStep = 25 * 1000;
+const maxInvestmentValue = 750 * 1000;
+const minInvestmentValue = 25 * 1000;
 
 const CalculatorPreview = ({ className, title, fact }) => {
   const [investment, setInvestment] = useState(25000);
@@ -27,27 +28,113 @@ const CalculatorPreview = ({ className, title, fact }) => {
 
   const calculatedRoi = useCallback(
     (years) => {
-      const interest = 0.5;
-      const calc = investment + (((cost * locations) / selloutRate) * years) / interest;
+      let val = 0;
+      let r = roi(investment, locations, cost, selloutRate);
+      if (years === 3) {
+        val = r[0];
+      }
 
-      return numberWithCommas(Math.round(calc));
+      if (years === 5) {
+        val = r[1];
+      }
+
+      return numberWithCommas(Math.round(val));
     },
     [investment, locations, cost, selloutRate]
   );
 
+  function roi(count, locations, adCost, selloutRate) {
+    console.log(count, locations, adCost, selloutRate);
+
+    let v1 = locations;
+    let v2 = adCost / 100;
+    let v3 = selloutRate;
+    let v4 = 2;
+
+    let rpd = (60 * v4 * v2 * v3) / 100;
+    let wdpy = 240;
+    let rpl = wdpy * rpd;
+
+    let yl = {
+      0: v1 * 0.13,
+      1: v1 * 0.38,
+      2: v1 * 1,
+      3: v1 * 1.05,
+      4: v1 * 1.1,
+    };
+
+    let yr = {
+      0: yl[0] * rpl,
+      1: yl[1] * rpl,
+      2: yl[2] * rpl,
+      3: yl[3] * rpl,
+      4: yl[4] * rpl,
+    };
+
+    let cost = 1600000;
+
+    let cy = {
+      0: cost,
+      1: cost * 1.5,
+      2: cost * 1.5 * 1.5,
+      3: cost * 1.5 * 1.5 * 1.25,
+      4: cost * 1.5 * 1.5 * 1.25,
+    };
+
+    let by = {};
+    for (let k = 0; k < 5; k++) {
+      if (k === 0) {
+        by[k] = yr[k] - cy[k];
+      } else {
+        by[k] = yr[k] + by[k - 1] - cy[k];
+      }
+    }
+
+    let re = {
+      0: 1,
+      1: 1,
+      2: 0.5,
+      3: 0.5,
+      4: 0.5,
+    };
+
+    let rvy = {};
+    for (let k = 0; k < 5; k++) {
+      if (k === 0) {
+        rvy[k] = by[k] * re[k];
+      } else {
+        rvy[k] = by[k] * re[k] + rvy[k - 1];
+      }
+    }
+
+    let dy = {};
+    for (let k = 0; k < 5; k++) {
+      dy[k] = by[k] / 2;
+    }
+
+    let owned = ((count / 60000) * 0.475) / 100;
+    let roi3 = dy[2] * owned;
+    let roi4 = dy[3] * owned;
+    let roi5 = dy[4] * owned;
+    console.log(roi3, roi3 + roi4 + roi5);
+    return [roi3, roi3 + roi4 + roi5];
+  }
+
   const plusMinusClick = useCallback(
     (type) => {
       let newValue = investment;
-
       if (type === 'plus') {
-        newValue += InvestmentStep;
-      } else if (type === 'minus') {
-        newValue -= InvestmentStep;
+        if (newValue <= maxInvestmentValue - InvestmentStep) {
+          newValue = investment + InvestmentStep;
+        }
       }
 
-      if (newValue > 0 && newValue < maxInvestmentValue) {
-        setInvestment(newValue);
+      if (type === 'minus') {
+        if (newValue >= minInvestmentValue + InvestmentStep) {
+          newValue = investment - InvestmentStep;
+        }
       }
+      setInvestment(newValue);
     },
     [investment, setInvestment]
   );
@@ -61,7 +148,7 @@ const CalculatorPreview = ({ className, title, fact }) => {
           <div className={styles.head}>
             <h1 className={styles.headTitle}>
               To determine your potential ROI, <i>adjust investment</i>
-              <img src={HeadDecorArrowSvg} className={styles.headDecor} />
+              <img src={HeadDecorArrowSvg} className={styles.headDecor} alt="" />
             </h1>
 
             <div className={styles.plusminus}>
@@ -97,7 +184,7 @@ const CalculatorPreview = ({ className, title, fact }) => {
                   Ad cost
                 </div>
                 <div className={styles.sectionContent}>
-                  <RangeSlider value={cost} min={1} step={1} max={100} onChange={(v) => setCost(v)} />
+                  <RangeSlider value={cost} min={20} step={1} max={40} onChange={(v) => setCost(v)} />
                 </div>
                 <div className={styles.sectionNote} data-tip="Cost per location">
                   <SvgIcon name="question" />
